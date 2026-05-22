3eae54638f059060d3b5ce7e9e2b6dd3911197d7
Snegopa007 <Snegopa007@gmail.com>
2026-05-22 09:55:03 +0500

    13) Шаблон пользователя №1

--- database/migrations/2026_04_29_045317_create_posts_table.php
+++ database/migrations/2026_04_29_045317_create_posts_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->integer('views')->unsigned()->default(0);
             $table->string('thumbnail')->nullable();
             $table->timestamps();
+            
         });
     }
 
